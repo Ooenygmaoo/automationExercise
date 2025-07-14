d6a7df3de4a059dbde3ed0e6859d18044e1b0731
dlope26 <56897053+Ooenygmaoo@users.noreply.github.com>
2025-07-14 17:35:29 -0500

    new locators5

--- build/test-results/timeline/report.js
+++ build/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"696d7501-f42d-4833-b64e-2b3d862b9194","feature":"User Registration Flow","scenario":"David completes account registration and deletion","start":1752530883824,"group":19,"content":"","tags":"","end":1752530898210,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"c5d18938-b93f-4c0d-a411-9ce50eb91faa","feature":"User Registration Flow","scenario":"David completes account registration and deletion","start":1752532468024,"group":19,"content":"","tags":"","end":1752532486435,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":19,"content":"Thread[ForkJoinPool-2-worker-1,5,main]"}]);
 });
